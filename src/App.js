edb52259fd3896d4672b5382ed5eed9973a4791c
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-06-02 21:04:49 -0400

    added notistack

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import "./App.css";
+import { useRef } from "react";
 import { PasswordConfirmationModalProvider } from "./ConfirmPasswordModal/ConfirmPasswordModalContext";
 import { MainView } from "./MainView";
 import { CertificateProvider } from "./CertificateProvider/CertificateProviderContext";
 import { WalletProvider } from "./WalletProvider/WalletProviderContext";
+import { SnackbarProvider } from "notistack";
+import { IconButton, makeStyles } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles((theme) => ({
+  snackbarClose: {
+    color: "#ffffff"
+  }
+}));
 
 function App() {
+  const notistackRef = useRef();
+  const classes = useStyles();
+
+  const onClickDismiss = (key) => () => {
+    notistackRef.current.closeSnackbar(key);
+  };
+
   return (
-    <PasswordConfirmationModalProvider>
-      <WalletProvider>
-        <CertificateProvider>
-          <MainView />
-        </CertificateProvider>
-      </WalletProvider>
-    </PasswordConfirmationModalProvider>
+    <SnackbarProvider
+      maxSnack={3}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      ref={notistackRef}
+      action={(key) => (
+        <IconButton onClick={onClickDismiss(key)} className={classes.snackbarClose}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    >
+      <PasswordConfirmationModalProvider>
+        <WalletProvider>
+          <CertificateProvider>
+            <MainView />
+          </CertificateProvider>
+        </WalletProvider>
+      </PasswordConfirmationModalProvider>
+    </SnackbarProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
