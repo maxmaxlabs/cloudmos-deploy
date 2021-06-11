9d4b339be1b416a8cec40706627374533181e6c5
Redm4x <max_maxime_max@hotmail.com>
2021-06-10 23:10:37 -0400

    Handle deployment without manifest

--- src/routes/DeploymentDetail/ManifestEditor.js
+++ src/routes/DeploymentDetail/ManifestEditor.js
@@ -25,7 +25,7 @@ export function ManifestEditor({ deployment, leases, closeManifestEditor }) {
 
   useEffect(() => {
     const deploymentData = getDeploymentLocalData(deployment.dseq);
-    setEditedManifest(deploymentData.manifest);
+    setEditedManifest(deploymentData?.manifest);
   }, [deployment]);
 
   function handleTextChange(value) {
