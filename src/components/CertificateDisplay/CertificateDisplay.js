8dbb6f8d679b21d53bf657b6ff1be4ba8aadd718
Redm4x <max_maxime_max@hotmail.com>
2021-06-20 02:12:27 -0400

    Fix certificate dates

--- src/components/CertificateDisplay/CertificateDisplay.js
+++ src/components/CertificateDisplay/CertificateDisplay.js
@@ -62,12 +62,12 @@ export function CertificateDisplay(props) {
   }
 
   function dateToStr(date) {
-    const year = date.getFullYear().toString().substring(2).padStart(2, "0");
-    const month = date.getMonth().toString().padStart(2, "0");
-    const day = date.getDay().toString().padStart(2, "0");
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const secs = date.getSeconds().toString().padStart(2, "0");
+    const year = date.getUTCFullYear().toString().substring(2).padStart(2, "0");
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const hours = date.getUTCHours().toString().padStart(2, "0");
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+    const secs = date.getUTCSeconds().toString().padStart(2, "0");
 
     return `${year}${month}${day}${hours}${minutes}${secs}Z`;
   }
