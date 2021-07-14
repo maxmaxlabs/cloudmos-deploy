bafe0bee3b2e3de0b135c6563e37def8deddf07a
Redm4x <max_maxime_max@hotmail.com>
2021-07-14 03:05:50 -0400

    Set deployment count limit to 200

--- src/shared/utils/apiUtils.js
+++ src/shared/utils/apiUtils.js
@@ -1,6 +1,6 @@
 export class ApiUrlService {
   static deploymentList(apiEndpoint, address) {
-    return apiEndpoint + "/akash/deployment/v1beta1/deployments/list?filters.owner=" + address;
+    return apiEndpoint + "/akash/deployment/v1beta1/deployments/list?filters.owner=" + address + "&pagination.limit=200";
   }
   static bidList(apiEndpoint, address, dseq) {
     return apiEndpoint + "/akash/market/v1beta1/bids/list?filters.owner=" + address + "&filters.dseq=" + dseq;
