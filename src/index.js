2db7d4a8a027c47e3f9131f024232d926b0be0ef
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-08-07 14:58:31 -0400

    bump version and run app after analytics

--- src/index.js
+++ src/index.js
@@ -12,57 +12,61 @@ import { initAnalytics } from "./shared/utils/analyticsUtils";
   const isDev = await window.electron.isDev();
   console.log(`isDev: ${isDev}`);
   initAnalytics(window.electron.isDev());
+
+  runApp();
 })();
 
-const appVersion = window.electron.getAppVersion();
-const appEnvironment = window.electron.getAppEnvironment();
+function runApp() {
+  const appVersion = window.electron.getAppVersion();
+  const appEnvironment = window.electron.getAppEnvironment();
 
-Sentry.init({
-  dsn: "https://fc8f0d800d664154a0f1babe0e318fbb@o877251.ingest.sentry.io/5827747",
-  environment: appEnvironment,
-  release: appVersion
-});
+  Sentry.init({
+    dsn: "https://fc8f0d800d664154a0f1babe0e318fbb@o877251.ingest.sentry.io/5827747",
+    environment: appEnvironment,
+    release: appVersion
+  });
 
-const theme = createMuiTheme({
-  palette: {
-    background: {
-      // default: "#282c34"
-    }
-    // type: "dark"
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        html: {
-          WebkitFontSmoothing: "auto"
+  const theme = createMuiTheme({
+    palette: {
+      background: {
+        // default: "#282c34"
+      }
+      // type: "dark"
+    },
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          html: {
+            WebkitFontSmoothing: "auto"
+          }
+        }
+      },
+      MuiInputBase: {
+        input: { padding: "10px 14px" }
+      },
+      MuiOutlinedInput: {
+        input: { padding: "10px 14px" }
+      },
+      MuiInputLabel: {
+        outlined: {
+          transform: "translate(14px, 12px) scale(1)"
         }
       }
-    },
-    MuiInputBase: {
-      input: { padding: "10px 14px" }
-    },
-    MuiOutlinedInput: {
-      input: { padding: "10px 14px" }
-    },
-    MuiInputLabel: {
-      outlined: {
-        transform: "translate(14px, 12px) scale(1)"
-      }
     }
-  }
-});
+  });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Sentry.ErrorBoundary fallback={({ error, resetError }) => <ErrorFallback error={error} resetErrorBoundary={resetError} />}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Sentry.ErrorBoundary>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  ReactDOM.render(
+    <React.StrictMode>
+      <Sentry.ErrorBoundary fallback={({ error, resetError }) => <ErrorFallback error={error} resetErrorBoundary={resetError} />}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Sentry.ErrorBoundary>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
