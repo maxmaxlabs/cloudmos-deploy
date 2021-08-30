15dfe4a6a485a8abf7f8c1348e6bb89ab0d7c177
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-08-29 20:33:49 -0400

    load node list and set default node from list

--- src/context/SettingsProvider/SettingsProviderContext.js
+++ src/context/SettingsProvider/SettingsProviderContext.js
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from "react";
-
-// const defaultRpcEndpoint = "http://rpc.akash.forbole.com";
-const defaultRpcEndpoint = "http://135.181.60.250:26657";
-const defaultApiEndpoint = "http://135.181.60.250:1317";
+import axios from "axios";
+import { mainNet } from "../../shared/contants";
 
 const SettingsProviderContext = React.createContext({});
 
 export const SettingsProvider = ({ children }) => {
-  const [settings, setSettings] = useState({ apiEndpoint: "", rpcEndpoint: "" });
+  const [settings, setSettings] = useState({ apiEndpoint: "", rpcEndpoint: "", isCustomNode: false, nodes: {} });
 
   // load settings from localStorage or set default values
   useEffect(() => {
-    const settingsStr = localStorage.getItem("settings");
-
-    if (settingsStr) {
+    const initiateSettings = async () => {
+      const settingsStr = localStorage.getItem("settings");
       const settings = JSON.parse(settingsStr);
-      setSettings(settings);
-    } else {
-      setSettings({ apiEndpoint: defaultApiEndpoint, rpcEndpoint: defaultRpcEndpoint });
-    }
+
+      if (settingsStr && settings.nodes) {
+        setSettings(settings);
+      } else {
+        // Set the available nodes list and default endpoints
+        const apiNodes = await axios.get(`${mainNet}/api-nodes.txt`);
+        const rpcNodes = await axios.get(`${mainNet}/rpc-nodes.txt`);
+        const _apiNodes = apiNodes.data
+          .split("\n")
+          .filter((x) => x)
+          .map((node) => new URL(node));
+        const _rpcNodes = rpcNodes.data
+          .split("\n")
+          .filter((x) => x)
+          .map((node) => new URL(node));
+        const nodes = {};
+
+        _apiNodes.forEach((node) => {
+          nodes[node.hostname] = {
+            api: node.port
+          };
+        });
+
+        _rpcNodes.forEach((node) => {
+          if (nodes[node.hostname]) {
+            nodes[node.hostname].rpc = node.port;
+          }
+        });
+
+        // Set first one as default
+        const firstKey = Object.keys(nodes)[0];
+        const defaultApiNode = `http://${firstKey}:${nodes[firstKey].api}`;
+        const defaultRpcNode = `http://${firstKey}:${nodes[firstKey].rpc}`;
+
+        setSettings({ apiEndpoint: defaultApiNode, rpcEndpoint: defaultRpcNode, nodes });
+      }
+    };
+
+    initiateSettings();
   }, []);
 
   const updateSettings = (settings) => {
