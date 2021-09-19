1865ea7d1e3fa7ba6af48525e27e89b72f04322f
Redm4x <max_maxime_max@hotmail.com>
2021-09-19 14:59:49 -0400

    Add lease selector

--- src/routes/DeploymentDetail/DeploymentLogs.js
+++ src/routes/DeploymentDetail/DeploymentLogs.js
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import { useCertificate } from "../../context/CertificateProvider";
-import { makeStyles, Checkbox, FormControlLabel, FormGroup, LinearProgress, Box } from "@material-ui/core";
+import { makeStyles, Checkbox, FormControl, InputLabel, Select, MenuItem, FormControlLabel, FormGroup, LinearProgress, Box } from "@material-ui/core";
 import { useProviders } from "../../queries";
 import MonacoEditor from "react-monaco-editor";
 import { ToggleButtonGroup, ToggleButton, Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
+  leaseSelector: {
+    margin: theme.spacing(1)
+  },
+  editor: {
+    marginTop: theme.spacing(1)
+  },
   root: {
     "& .MuiToggleButton-root": {
       color: "rgba(0, 0, 0, 0.54)",
@@ -21,12 +27,13 @@ const useStyles = makeStyles((theme) => ({
 export function DeploymentLogs({ leases }) {
   const [logs, setLogs] = useState([]);
   const [isWaitingForFirstLog, setIsWaitingForFirstLog] = useState(true);
+  //const [leaseStatus, setLeaseStatus] = useState(null);
   const [services, setServices] = useState([]);
   const [selectedServices, setSelectedServices] = useState([]);
   const [selectedMode, setSelectedMode] = useState("logs");
   const [stickToBottom, setStickToBottom] = useState(true);
 
-  //const [selectedLease, setSelectedLease] = useState(null);
+  const [selectedLease, setSelectedLease] = useState(null);
 
   const classes = useStyles();
   const { data: providers } = useProviders();
@@ -36,56 +43,62 @@ export function DeploymentLogs({ leases }) {
   const monacoRef = useRef();
 
   useEffect(() => {
+    if (!leases || leases.length === 0) return;
+
+    setSelectedLease(leases[0]);
+  }, [leases]);
+
+  useEffect(() => {
+    if (!selectedLease) return;
+
+    (async () => {
+      const providerInfo = providers?.find((p) => p.owner === selectedLease.provider);
+
+      const leaseStatusPath = `${providerInfo.host_uri}/lease/${selectedLease.dseq}/${selectedLease.gseq}/${selectedLease.oseq}/status`;
+      const leaseStatus = await window.electron.queryProvider(leaseStatusPath, "GET", null, localCert.certPem, localCert.keyPem);
+
+      setServices(Object.keys(leaseStatus.services));
+      setSelectedServices(Object.keys(leaseStatus.services));
+    })();
+  }, [selectedLease]);
+
+  useEffect(() => {
     if (!providers) return;
     if (!isLocalCertMatching) return;
+    if (!selectedLease) return;
 
-    let sockets = [];
-    if (leases && leases.length > 0) {
-      (async () => {
-        const lease = leases[0];
-        const providerInfo = providers?.find((p) => p.owner === lease.provider);
+    setLogs([]);
 
-        // TODO: Do only one time?
-        const leaseStatusPath = `${providerInfo.host_uri}/lease/${lease.dseq}/${lease.gseq}/${lease.oseq}/status`;
-        const leaseStatus = await window.electron.queryProvider(leaseStatusPath, "GET", null, localCert.certPem, localCert.keyPem);
+    const providerInfo = providers?.find((p) => p.owner === selectedLease.provider);
 
-        let url = null;
-        if (selectedMode === "logs") {
-          setServices(Object.keys(leaseStatus.services));
-          setSelectedServices(Object.keys(leaseStatus.services));
-
-          url = `${providerInfo.host_uri}/lease/${lease.dseq}/${lease.gseq}/${lease.oseq}/logs?follow=true`;
-        } else {
-          url = `${providerInfo.host_uri}/lease/${lease.dseq}/${lease.gseq}/${lease.oseq}/kubeevents?follow=true`;
-        }
-
-        const socket = window.electron.openWebSocket(url, localCert.certPem, localCert.keyPem, (message) => {
-          setIsWaitingForFirstLog(false);
-
-          let parsedLog = null;
-          if (selectedMode === "logs") {
-            console.log(message);
-            parsedLog = JSON.parse(message);
-            parsedLog.service = parsedLog.name.split("-")[0];
-          } else {
-            console.log(message);
-            //parsedLog = {} =
-            parsedLog = JSON.parse(message);
-            parsedLog.message = parsedLog.event;
-          }
-          //parsedLog.message = parsedLog.note;
-          setLogs((logs) => [...logs, parsedLog]);
-        });
-        sockets.push(socket);
-      })();
+    let url = null;
+    if (selectedMode === "logs") {
+      url = `${providerInfo.host_uri}/lease/${selectedLease.dseq}/${selectedLease.gseq}/${selectedLease.oseq}/logs?follow=true`;
+    } else {
+      url = `${providerInfo.host_uri}/lease/${selectedLease.dseq}/${selectedLease.gseq}/${selectedLease.oseq}/kubeevents?follow=true`;
     }
 
-    return () => {
-      for (let socket of sockets) {
-        socket.close();
+    const socket = window.electron.openWebSocket(url, localCert.certPem, localCert.keyPem, (message) => {
+      setIsWaitingForFirstLog(false);
+
+      let parsedLog = null;
+      if (selectedMode === "logs") {
+        parsedLog = JSON.parse(message);
+        parsedLog.service = parsedLog.name.split("-")[0];
+        parsedLog.message = parsedLog.service + ": " + parsedLog.message;
+      } else {
+        parsedLog = JSON.parse(message);
+        parsedLog.service = parsedLog.object.name.split("-")[0];
+        parsedLog.message = `${parsedLog.service}: ${parsedLog.time} [${parsedLog.type}] [${parsedLog.reason}] [${parsedLog.object.kind}] ${parsedLog.note}`;
       }
+
+      setLogs((logs) => [...logs, parsedLog]);
+    });
+
+    return () => {
+      socket.close();
     };
-  }, [leases, providers, isLocalCertMatching, selectedMode]);
+  }, [leases, providers, isLocalCertMatching, selectedMode, selectedLease]);
 
   const logText = logs
     .filter((x) => selectedServices.includes(x.service))
@@ -121,34 +134,51 @@ export function DeploymentLogs({ leases }) {
     }
   }
 
+  function handleLeaseChange(ev, val) {
+    setSelectedLease(leases.find((x) => x.id === val));
+  }
+
   return (
     <div className={classes.root}>
       {isLocalCertMatching ? (
         <>
-          {/* {leases.map(l => (
-            <>{JSON.stringify(l)}</>
-          ))} */}
-          <ToggleButtonGroup color="primary" value={selectedMode} exclusive onChange={handleModeChange}>
-            <ToggleButton value="logs">Logs</ToggleButton>
-            <ToggleButton value="events">Events</ToggleButton>
-          </ToggleButtonGroup>
-          <FormGroup row>
-            {services.map((service) => (
+          {selectedLease && (
+            <>
+              {leases.length > 1 && (
+                <ToggleButtonGroup className={classes.leaseSelector} color="primary" value={selectedLease.id} exclusive onChange={handleLeaseChange}>
+                  {leases.map((l) => (
+                    <ToggleButton key={l.id} value={l.id}>
+                      GSEQ: {l.gseq}
+                    </ToggleButton>
+                  ))}
+                </ToggleButtonGroup>
+              )}
+
+              <ToggleButtonGroup color="primary" value={selectedMode} exclusive onChange={handleModeChange}>
+                <ToggleButton value="logs">Logs</ToggleButton>
+                <ToggleButton value="events">Events</ToggleButton>
+              </ToggleButtonGroup>
+              {services.length > 1 && (
+                <FormGroup row>
+                  {services.map((service) => (
+                    <FormControlLabel
+                      key={service}
+                      control={
+                        <Checkbox color="primary" checked={selectedServices.includes(service)} onChange={(ev) => setServiceCheck(service, ev.target.checked)} />
+                      }
+                      label={service}
+                    />
+                  ))}
+                </FormGroup>
+              )}
+              {isWaitingForFirstLog && <LinearProgress />}
+              <MonacoEditor ref={monacoRef} className={classes.editor} height="600" theme="vs-dark" value={logText} options={options} />
               <FormControlLabel
-                key={service}
-                control={
-                  <Checkbox color="primary" checked={selectedServices.includes(service)} onChange={(ev) => setServiceCheck(service, ev.target.checked)} />
-                }
-                label={service}
+                control={<Checkbox color="primary" checked={stickToBottom} onChange={(ev) => setStickToBottom(ev.target.checked)} />}
+                label={"Stick to bottom"}
               />
-            ))}
-          </FormGroup>
-          {isWaitingForFirstLog && <LinearProgress />}
-          <MonacoEditor ref={monacoRef} height="600" theme="vs-dark" value={logText} options={options} />
-          <FormControlLabel
-            control={<Checkbox color="primary" checked={stickToBottom} onChange={(ev) => setStickToBottom(ev.target.checked)} />}
-            label={"Stick to bottom"}
-          />
+            </>
+          )}
         </>
       ) : (
         <Box mt={1}>
