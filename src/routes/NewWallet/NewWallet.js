0743e28379c84879595c0ba1accd4d32d98ad94e
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2021-10-01 20:30:10 -0400

    show selected words

--- src/routes/NewWallet/NewWallet.js
+++ src/routes/NewWallet/NewWallet.js
@@ -1,19 +1,19 @@
 import { Button, makeStyles, Container, Typography, ButtonGroup, TextareaAutosize, Box } from "@material-ui/core";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UrlService } from "../../shared/utils/urlUtils";
 import { generateNewWallet } from "../../shared/utils/walletUtils";
 import Alert from "@material-ui/lab/Alert";
 
 const useStyles = makeStyles((theme) => ({
-  root: { padding: "10% 0" },
+  root: { padding: "5% 0" },
   title: {
     fontWeight: "bold",
     fontSize: "3rem",
     textAlign: "center"
   },
   container: {
-    paddingTop: "3rem",
+    paddingTop: "2rem",
     display: "flex",
     flexDirection: "column"
   },
@@ -31,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
     "& li": {
       listStyle: "disc"
     }
+  },
+  wordButton: {
+    margin: "2px"
   }
 }));
 
@@ -39,21 +42,26 @@ export function NewWallet() {
   const [numberOfWords, setNumberOfWords] = useState(12);
   const [newWallet, setNewWallet] = useState(null);
   const [isGeneratingNewWallet, setIsGeneratingNewWallet] = useState(false);
+  const [isKeyValidating, setIsKeyValidating] = useState(false);
+  const [isKeyValidated, setIsKeyValidated] = useState(false);
+  const [selectedWords, setSelectedWords] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     const init = async () => {
       await genNewWallet(numberOfWords);
     };
 
-    init();
+    if (!isKeyValidating) {
+      init();
+    }
   }, []);
 
   const genNewWallet = async (numOfWords) => {
     setIsGeneratingNewWallet(true);
     const wallet = await generateNewWallet(numOfWords);
     setNewWallet(wallet);
-    const accounts = await wallet.getAccounts();
-    debugger;
+    // const accounts = await wallet.getAccounts();
 
     setIsGeneratingNewWallet(false);
   };
@@ -63,7 +71,41 @@ export function NewWallet() {
     await genNewWallet(number);
   };
 
-  const onCreateWallet = () => {};
+  const onWordClick = (word, shouldAdd) => {
+    setSelectedWords((prevWords) => {
+      let newWords;
+      if (shouldAdd) {
+        newWords = prevWords.concat([word]);
+      } else {
+        newWords = prevWords.filter((w) => w !== word);
+      }
+
+      if (newWords.length === newWallet.mnemonic.split(" ").length) {
+        // TODO validate the order of the words
+        setIsKeyValidated(true);
+      }
+
+      return newWords;
+    });
+  };
+
+  const onBackClick = () => {
+    if (isKeyValidating) {
+      setIsKeyValidating(false);
+      setIsKeyValidated(false);
+      setSelectedWords([]);
+    } else {
+      history.push(UrlService.register());
+    }
+  };
+
+  const onCreateWallet = () => {
+    if (isKeyValidated) {
+      history.replace(UrlService.dashboard());
+    } else {
+      setIsKeyValidating(true);
+    }
+  };
 
   return (
     <div className={classes.root}>
@@ -72,39 +114,64 @@ export function NewWallet() {
       </Typography>
 
       <Container maxWidth="xs" className={classes.container}>
-        <Alert variant="filled" color="error" className={classes.alert} icon={false}>
-          <Typography>Backup your mnemonic seed securely</Typography>
+        {isKeyValidating ? (
+          <>
+            <div>
+              {selectedWords.map((word) => (
+                <Button onClick={() => onWordClick(word, false)} variant="contained" color="primary" className={classes.wordButton}>
+                  {word}
+                </Button>
+              ))}
+            </div>
 
-          <ul className={classes.alertList}>
-            <li>Anyone with your mnemonic seed can take your assets.</li>
-            <li>Lost mnemonic seed can't be recovered.</li>
-          </ul>
-        </Alert>
+            <hr />
 
-        <Box display="flex" justifyContent="space-between" marginBottom="1rem">
-          <Typography variant="h5">Mnemonic Seed</Typography>
-          <ButtonGroup variant="contained" size="small">
-            <Button onClick={() => onNumberChange(12)} size="small" color={numberOfWords === 12 ? "primary" : "default"}>
-              12 words
-            </Button>
-            <Button onClick={() => onNumberChange(24)} size="small" color={numberOfWords === 24 ? "primary" : "default"}>
-              24 words
-            </Button>
-          </ButtonGroup>
-        </Box>
+            <div>
+              {newWallet.mnemonic
+                .split(" ")
+                .filter((w) => !selectedWords.some((_) => _ === w))
+                .map((word) => (
+                  <Button onClick={() => onWordClick(word, true)} className={classes.wordButton} variant="outline" size="small">
+                    {word}
+                  </Button>
+                ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <Alert variant="filled" color="error" className={classes.alert} icon={false}>
+              <Typography>Backup your mnemonic seed securely</Typography>
 
-        <div>
-          <TextareaAutosize value={newWallet?.mnemonic} className={classes.textArea} rowsMin={5} contentEditable={false} />
-        </div>
+              <ul className={classes.alertList}>
+                <li>Anyone with your mnemonic seed can take your assets.</li>
+                <li>Lost mnemonic seed can't be recovered.</li>
+              </ul>
+            </Alert>
 
-        <Button component={Link} to={UrlService.register()} variant="contained" color="primary" disabled={isGeneratingNewWallet}>
+            <Box display="flex" justifyContent="space-between" marginBottom="1rem">
+              <Typography variant="h5">Mnemonic Seed</Typography>
+              <ButtonGroup variant="contained" size="small">
+                <Button onClick={() => onNumberChange(12)} size="small" color={numberOfWords === 12 ? "primary" : "default"}>
+                  12 words
+                </Button>
+                <Button onClick={() => onNumberChange(24)} size="small" color={numberOfWords === 24 ? "primary" : "default"}>
+                  24 words
+                </Button>
+              </ButtonGroup>
+            </Box>
+
+            <div>
+              <TextareaAutosize value={newWallet?.mnemonic} className={classes.textArea} rowsMin={5} contentEditable={false} />
+            </div>
+          </>
+        )}
+
+        <Button variant="contained" color="primary" disabled={isGeneratingNewWallet || (isKeyValidating && !isKeyValidated)} onClick={onCreateWallet}>
           Next
         </Button>
 
         <Box marginTop="1rem">
-          <Button component={Link} to={UrlService.register()}>
-            Back
-          </Button>
+          <Button onClick={onBackClick}>Back</Button>
         </Box>
       </Container>
     </div>
