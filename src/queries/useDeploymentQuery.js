8a3f3919b987729768aecda6f92bffd315f569fb
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2022-03-28 16:18:36 -0400

    paginated loading of deployments and providers

--- src/queries/useDeploymentQuery.js
+++ src/queries/useDeploymentQuery.js
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
 import { QueryKeys } from "./queryKeys";
 import axios from "axios";
-import { ApiUrlService } from "../shared/utils/apiUtils";
+import { ApiUrlService, loadWithPagination } from "../shared/utils/apiUtils";
 import { deploymentToDto } from "../shared/utils/deploymentDetailUtils";
 import { useSettings } from "../context/SettingsProvider";
 
@@ -9,10 +9,9 @@ import { useSettings } from "../context/SettingsProvider";
 async function getDeploymentList(apiEndpoint, address) {
   if (!address) return [];
 
-  const response = await axios.get(ApiUrlService.deploymentList(apiEndpoint, address));
-  let deployments = response.data;
+  const deployments = await loadWithPagination(ApiUrlService.deploymentList(apiEndpoint, address), "deployments", 1000);
 
-  return deployments.deployments.map((d) => deploymentToDto(d));
+  return deployments.map((d) => deploymentToDto(d));
 }
 
 export function useDeploymentList(address, options) {
